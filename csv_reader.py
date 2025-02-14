b8d6edf327565c7244ca5c3a35a90cadce0ca838
demvlad <demvlad2@yandex.ru>
2025-02-15 01:34:08 +0300

    added using of plane aerodynamics data

--- csv_reader.py
+++ csv_reader.py
@@ -1,23 +1,31 @@
 import numpy as np
 from headers import *
 
-def find_header_row(file_path, identifier):
+def find_header_row(file_path, identifier, bf_settings = None):
     with open(file_path, 'r') as file:
         for i, line in enumerate(file):
             # Strip any leading/trailing whitespace and quotes from the line
             stripped_line = line.strip().strip('"').strip("'")
+            # get BF settings dictionary
+            if bf_settings != None:
+                values = line.split(",")
+                if len(values) == 2:
+                    values[0] = values[0].strip().strip('"').strip("'")
+                    values[1] = values[1].strip().strip('"').strip("'")
+                    bf_settings[values[0]] = values[1]
             if stripped_line.startswith(identifier):
                 return i
     return None
 
 def read_csv_as_dict(file_path):
     identifier = "loopIteration"
-    # Find the row where the header starts
-    header_row = find_header_row(file_path, identifier)
-    
+    bf_settings = {}
+    # Find the row where the header starts, get BF settings dictionary
+    header_row = find_header_row(file_path, identifier, bf_settings)
+
     if header_row is None:
         raise ValueError(f"Header row with identifier '{identifier}' not found in the file.")
-    
+
     # Read the header row to get the column names
     column_names = np.genfromtxt(file_path, delimiter=',', skip_header=header_row, max_rows=1, dtype=str)
     # Strip quotes from column names
@@ -25,17 +33,17 @@ def read_csv_as_dict(file_path):
 
     data_list = []
     total_lines = sum(1 for _ in open(file_path)) - (header_row + 1)
-    
+
     with open(file_path, 'r') as file:
         for i, line in enumerate(file):
             if i <= header_row:
                 continue
             data_list.append(np.array(line.strip().split(','), dtype=float))
-            
+
             # Print progress
             if i % 1000 == 0 or i == total_lines + header_row:
                 print(f"Reading file: {i - header_row} / {total_lines} rows")
-    
+
     # Convert list of arrays to a numpy array
     data = np.vstack(data_list)
 
@@ -55,7 +63,7 @@ def read_csv_as_dict(file_path):
     # Adjust the "time" column to start from zero
     time_values = data_dict[header_time]
     data_dict[header_time] = (data_dict[header_time] - data_dict[header_time][0]) / 1e6
-    
+
     # Calculate the average time interval (dt) in seconds
     num_intervals = len(data_dict[header_time]) - 1
     if num_intervals > 0:
@@ -63,5 +71,7 @@ def read_csv_as_dict(file_path):
         data_dict["dt"] = dt
 
     data_dict["total_lines"] = total_lines
+    acc_1G = int(bf_settings["acc_1G"])
+    data_dict[header_accel_z] = data_dict[header_accel_z] / acc_1G
 
     return data_dict
--- headers.py
+++ headers.py
@@ -9,3 +9,9 @@ header_voltage = "vbatLatest"
 
 header_pitch_multiplier_to_rad = math.pi/180 / 10
 header_voltage_multiplier_to_volts = 1/100
+
+# data for aerodynamics calculate
+header_accel_z = "accSmooth[2]"
+header_const_plane_mass = "plane_mass"
+header_const_wing_load = "wing_load"
+header_const_air_density = "air_density"
